10b2b101937e6f7a0024cadf8041ef40681dccf1
suzuka9321 <suzuka9321@gmail.com>
2018-01-08 14:24:56 +0900

    Crate global variable

---
+++ config.py
@@ -0,0 +1,24 @@
+# coding:shift-jis
+
+# ƒOƒ[ƒoƒ‹•Ï”
+
+# 1 | 2 | 3
+#---+---+---
+# 4 | 5 | 6
+#---+---+---
+# 7 | 8 | 9
+
+
+left_over = 0      # 1
+center_over = 0    # 2
+right_over = 0     # 3
+
+left_middle = 0    # 4
+center_middle = 0  # 5
+right_middle = 0   # 6
+
+left_under = 0     # 7
+center_under = 0   # 8
+right_under = 0    # 9
+
+pass_count = 0
